c03c6875d8380a4ac3d3076639dac44b2db90797
Anton Buza <Buzaant@mail.ru>
2023-03-26 20:12:01 +0700

    Create function inter user information

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int TextForLength(string text)
+{
+    System.Console.WriteLine(text);
+    string message = Console.ReadLine();
+    int result = Convert.ToInt32(message);
+    return result;
+}
+
+string TextForArray(string text)
+{
+    System.Console.WriteLine(text);
+    string message = Console.ReadLine();
+    return message;
+}
